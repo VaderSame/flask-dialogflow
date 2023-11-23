04d2fe1fcea7a3e61cd49c69f72a72dd56d02e03
Semal Shastri <shemalshastri47@gmail.com>
2023-11-23 15:53:01 -0500

    init

---
+++ test.py
@@ -0,0 +1,34 @@
+## Render, Github, Flask and DialogFlow integration Example.
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from uuid import uuid4
+import os
+
+
+
+
+# Determine environment
+prod = True if os.getenv("PRODUCTION") == "production" else False
+
+print(prod)
+
+# Create Flask App
+app = Flask(__name__)
+
+
+
+
+
+@app.route("/test", methods=["POST"])
+def example_route():
+    data = request.get_data(as_text=True)
+    print(data)
+ 
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    if prod:
+        app.run(debug=True)
+    else:
+        app.run(debug=True, host="127.0.0.1", port=8080)
