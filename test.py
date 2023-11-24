45b1805ecb9ffdc708c4a78e4587b10eef24eec8
Semal Shastri <shemalshastri47@gmail.com>
2023-11-23 19:49:03 -0500

    get json parameter changed

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 ## Render, Github, Flask and DialogFlow integration Example.
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+from google.api_core.exceptions import InvalidArgument
+from google.cloud.dialogflow_v2 import SessionsClient
+from google.cloud.dialogflow_v2.types import TextInput, QueryInput
 from uuid import uuid4
 import os
 
+# Set google credentials
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+project_id = "restrobud-curs"
+dialogflow_lang_code = "en"
+session_id = uuid4()
+
+# Create DialogFlow Session Client
+session_client = SessionsClient()
+session = session_client.session_path(project_id, session_id)
+# text_input = TextInput(text = "Hello" , language_code = dialogflow_lang_code)
+# query_input = QueryInput(text = text_input)
 
 
 
@@ -15,18 +29,37 @@ print(prod)
 # Create Flask App
 app = Flask(__name__)
 
+@app.route("/test-df", methods=["POST"])
+def example_df_route():
+    data = request.get_json(silent=True)
+    message = data["message"]
 
+    print(message)
+
+    text_input = TextInput(text=message, language_code=dialogflow_lang_code)
+    query_input = QueryInput(text=text_input)
+
+    try:
+        response = session_client.detect_intent(
+            session=session, query_input=query_input
+        )
+    except InvalidArgument as e:
+        return f"Failed to parse result {e}"
+
+    print("Fulfillment text:", response.query_result.fulfillment_text)
+
+    return jsonify({"response": response.query_result.fulfillment_text})
 
 
 
 @app.route("/test", methods=["POST"])
 def example_route():
-    data = request.get_data(as_text=True)
+    data = request.get_json(silent=True)
     print(data)
  
     return jsonify(data)
 
-#  make sure the app is running , postman request working , render request fetch
+
 if __name__ == "__main__":
     if prod:
         app.run(debug=True)
